80f2ba2b35623b22e0ff404bce8c798a25fd31b4
EnzoMSantos <enzosantos2502@gmail.com>
2023-05-16 17:45:05 -0300

    criando a var

--- advinhação/programa.c
+++ advinhação/programa.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
+
+
     printf("*****************************************\n");
     printf("* Bem vindo ao nosso jogo de advinhacao *\n");
     printf("*****************************************\n");
+
+    int secretNumber = 42;
 }
