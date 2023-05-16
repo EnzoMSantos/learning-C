c1754c6baa878334c58f3977199020710f263db1
EnzoMSantos <enzosantos2502@gmail.com>
2023-05-16 18:04:03 -0300

    fazendo ifs e elses

--- advinhação/programa.c
+++ advinhação/programa.c
@@ -8,4 +8,22 @@ int main() {
     printf("*****************************************\n");
 
     int secretNumber = 42;
+
+    int chute;
+
+    printf("Qual o seu chute?");
+    scanf("%d", &chute);
+
+    printf("seu chute: %d\n", chute);
+
+    if(chute == secretNumber){
+    printf("Voce acertou, Parabens\n"); 
+    }
+    else {
+        printf("Voce errou!\n");
+        printf("Tente de novo!");
+    }
+
+   
+
 }
