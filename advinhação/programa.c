13a0a28f850f25b27f0c982747b22dce959db652
EnzoMSantos <enzosantos2502@gmail.com>
2023-05-17 20:02:45 -0300

    ajustando as tentativas, deixando inf, mas dizendo quantas tentativas ele teve

--- advinhação/programa.c
+++ advinhação/programa.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 int main() {
 
 
@@ -9,38 +11,47 @@ int main() {
 
     int secretNumber = 42;
 
+    
     int chute;
+    
+    int tentativas = 0;
 
-    for(int i = 1; i <= 3; i++){
+    while(1){
 
-        printf("Tentativa %d de 3\n", i);
+        printf("Tentativa %d\n", tentativas);
         printf("Qual o seu chute? ");    
         scanf("%d", &chute);
-
         printf("seu chute: %d\n", chute);
 
+        if(chute <0) {
+            printf("Voce nao pode chutar numeros negativos\n"); 
+
+            continue;
+        }
+
         int acertou = (chute == secretNumber);
+        int maior = chute > secretNumber;
+        
+        if(acertou) {
+            printf("Parabées! Voce acertou!\n");
+            printf("Jogue de novo, voce e um bom jogador!\n");
 
-        if(acertou){
-            printf("Voce acertou, Parabens\n"); 
+            break;
         }
-        else {
-            printf("Voce errou!\n");
 
-            int maior = chute > secretNumber;
-            if(maior){
-                printf("Seu chute foi maior que seu numero secreto.\n");
-            }
-            else {
-                printf("Seu chute foi menor que seu numero secreto.\n");
-            }
-            
-            
-            printf("Tente de novo!\n");
+        else if(maior) {
+            printf("Seu chute foi maior que o numero secreto\n");
         }
+
+        else{
+            printf("Seu chute foi menor que o numero secreto\n");
+        }
+        tentativas++;
     }
+
+    printf("Fim de Jogo!"); 
+    printf("Voce acertou em %d tentativas!\n", tentativas);
     
-    printf("Fim de Jogo!");
    
 
 }
